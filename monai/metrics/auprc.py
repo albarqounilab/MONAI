c825d92eab99aa03bc9e8123478f3ce4a3d8fc77
Cosmin I. Bercea <cosmin.bercea@helmholtz-muenchen.de>
2021-07-20 09:55:22 +0200

    updated prc metric

--- monai/metrics/auprc.py
+++ monai/metrics/auprc.py
@@ -4,9 +4,33 @@ Computes Area Under the Precision-Recall Curve (AUPRC). Referring to:
 The input `y_pred` and `y` can be a list of `channel-first` Tensor or a `batch-first` Tensor.
 """
 from sklearn.metrics import precision_recall_curve, average_precision_score
+import torch
+from .metric import IterationMetric
 
 
-def compute_prc(predictions, labels):
-    precisions, recalls, thresholds = precision_recall_curve(labels.astype(int), predictions)
-    auprc = average_precision_score(labels.astype(int), predictions)
+class PRCMetric(IterationMetric):
+    """
+    Computes Area Under the Precision-Recall Curve (AUPRC). Referring to:
+    `sklearn.metrics.precision_recall_curve, average_precision_score
+    The input `y_pred` and `y` can be a list of `channel-first` Tensor or a `batch-first` Tensor.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def _compute_tensor(self, y_pred: torch.Tensor, y: torch.Tensor = None):  # type: ignore
+        return compute_prc(y_pred, y)
+
+
+def compute_prc(y_pred, y):
+    y_pred = y_pred.cpu().detach().numpy()
+    y = y.cpu().detach().numpy()
+    precisions, recalls, thresholds = precision_recall_curve(y.astype(int), y_pred)
+    auprc = average_precision_score(y.astype(int), y_pred)
     return auprc, precisions, recalls, thresholds
+
+
+
+
+
+
