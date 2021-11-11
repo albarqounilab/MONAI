0dda966820cb079db7d51d5de9d0e26fef38b937
tobias.bernecker <tobias.bernecker@helmholtz-muenchen.de>
2021-11-11 01:01:06 +0100

    MONAI/monai/losses/alignment_loss.py: - Added exception handling for non-positive definite covariance matrices

--- monai/losses/alignment_loss.py
+++ monai/losses/alignment_loss.py
@@ -1,4 +1,5 @@
 import torch
+import logging
 
 from torch.nn import Module
 from torch.distributions import MultivariateNormal, kl_divergence
@@ -36,17 +37,24 @@ class StatisticsAlignmentLoss(Module):
             loss: torch.FloatTensor
                 Loss value.
         """
-        # Create multivariate normal distribution of features from client_name
-        current_dist = MultivariateNormal(current_mean, current_cov)
         alignment_loss = torch.tensor(0, device=self.device)
-        for name in client_stats:
-            # Only align to feature statistics of other clients
-            if name != client_name:
-                mean, cov = client_stats[name]
-                mean, cov = mean.to(self.device), cov.to(self.device)
-                # Create multivariate normal distribution of features from other client
-                dist = MultivariateNormal(mean, cov)
-                # NOTE: Use forward KL divergence (https://agustinus.kristia.de/techblog/2016/12/21/forward-reverse-kl/)
-                alignment_loss = alignment_loss + kl_divergence(dist, current_dist)
+        try:
+            # Create multivariate normal distribution of features from client_name
+            current_dist = MultivariateNormal(current_mean, current_cov)
+            for name in client_stats:
+                # Only align to feature statistics of other clients
+                if name != client_name:
+                    mean, cov = client_stats[name]
+                    mean, cov = mean.to(self.device), cov.to(self.device)
+                    try:
+                        # Create multivariate normal distribution of features from other client
+                        dist = MultivariateNormal(mean, cov)
+                        # NOTE: Use forward KL divergence
+                        # (see https://agustinus.kristia.de/techblog/2016/12/21/forward-reverse-kl/)
+                        alignment_loss = alignment_loss + kl_divergence(dist, current_dist)
+                    except ValueError:
+                        logging.info(f'Covariance matrix of features from client {name} not positive definite!')
+        except ValueError:
+            logging.info(f'Covariance matrix of features from current client {client_name} not positive definite!')
         alignment_loss = alignment_loss / (len(client_stats) - 1)
         return alignment_loss
